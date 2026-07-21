b79b09cf72f7000a8612909c67a0731c448607ee
seeram17 <seeramkarthikeya@gmail.com>
2026-07-21 16:34:27 +0530

    Refactor product rendering to use Product class methods for image URL and price formatting

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -42,7 +42,7 @@ export function renderOrderSummary(){
                     ${product.name}
                   </div>
                   <div class="product-price">
-                    ${formatCurrency(product.priceCents)}
+                    ${product.getPrice()}
                   </div>
 
                   <div class="product-quantity js-product-quantity${product.id}">
